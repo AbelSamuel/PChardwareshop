a866ef723cdeec02edbdb32260468d04881d94b3
Kevin Hoang <kevinhoang32@yahoo.com>
2017-08-01 13:17:19 -0400

    Changed text on main screen

--- view/header.php
+++ view/header.php
@@ -2,11 +2,11 @@
 <html>
 <!-- the head section -->
 <head>
-    <title>My Guitar Shop</title>
+    <title>PC Hardware Shop</title>
     <link rel="stylesheet" type="text/css"
           href="/book_apps/ch05_guitar_shop/main.css">
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>My Guitar Shop</h1></header>
+<header><h1>PC Hardware Shop</h1></header>
