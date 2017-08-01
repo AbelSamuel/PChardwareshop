e4f26ff79eb3844dd04f1fb5d667687b4c5f74c6
Kevin Hoang <kevinhoang32@yahoo.com>
2017-08-01 13:43:10 -0400

    new changes

--- view/footer.php
+++ view/footer.php
@@ -1,10 +1,9 @@
 <footer>
     <p class="copyright">
-<<<<<<< HEAD
-        &copy; <?php echo date("Y"); ?> PC Hardware Shop, Inc.
-=======
+
+
         &copy; <?php echo date("Y"); ?> Kyle Poitinger, Abel Samuel, Kevin Hoang.
->>>>>>> f24f8be2d10477d092ee19baaca02cd093383cda
+
     </p>
 </footer>
 </body>
