4a337c5639f01a84d2fe9dcb921f23670b848a6b
Kyle-Poitinger <kpoiting@uncc.edu>
2017-08-04 17:44:01 -0400

    changes

--- cart/index.php
+++ cart/index.php
@@ -2,7 +2,7 @@
 require('../model/database.php');
 require('../model/product_db.php');
 require('../model/category_db.php');
-require('../model/cart.db.php');
+require('../model/cart_db.php');
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
