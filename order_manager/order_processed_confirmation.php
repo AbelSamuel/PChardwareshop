cab93aee196e2f289a74580ff76d7e2b4abf692a
Kyle-Poitinger <kpoiting@uncc.edu>
2017-08-04 16:51:19 -0400

    changes

---
+++ order_manager/order_processed_confirmation.php
@@ -0,0 +1,9 @@
+<?php include '../view/header.php'; ?>
+<main>    
+    <section>
+        <h1>Thank you for processing our order!></h1>
+        <a href="/PChardwareshop/index.php">Home</a>
+        
+    </section>
+</main>
+<?php include '../view/footer.php'; ?>
