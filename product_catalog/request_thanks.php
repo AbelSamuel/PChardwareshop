532c4c88b13dc1183c90a402eb300b45cc4f347a
Kyle-Poitinger <kpoiting@uncc.edu>
2017-08-04 22:20:20 -0400

    changes

--- product_catalog/request_thanks.php
+++ product_catalog/request_thanks.php
@@ -1,23 +1,5 @@
 <?php include '../view/header.php'; ?>
 <main>
-    <aside>
-        <h1>Categories</h1>
-
-        <nav>
-            <ul>
-                <!-- display links for all categories -->
-                <?php foreach($categories as $category) : ?>
-                <li>
-                    <a href="?category_id=<?php 
-                              echo $category['categoryID']; ?>">
-                        <?php echo $category['categoryName']; ?>
-                    </a>
-                </li>
-                <?php endforeach; ?>
-                <a href="/PChardwareshop/index.php">Home</a>
-            </ul>
-        </nav>
-    </aside>
     <section>
         <h1>Thank you for requesting more stock, we are sorry for the inconvenience!</h1>
         <p>We look forward to adding "Add to Cart" functionality in the near future!</p>       
